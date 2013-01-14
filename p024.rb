47862e294ec4190cce521993d7023743d47340df
Tim Yang <timothy.d.yang@gmail.com>
2013-01-14 01:20:29 -0600

    add p024

---
+++ p024.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+#
+# Lexicographic permutations
+# http://projecteuler.net/problem=24
+#
+# A permutation is an ordered arrangement of objects. For example, 3124 is one
+# possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
+# are listed numerically or alphabetically, we call it lexicographic order. The
+# lexicographic permutations of 0, 1 and 2 are:
+#
+#   012   021   102   120   201   210
+#
+# What is the millionth lexicographic permutation of the digits
+# 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+#
+
+p "0123456789".chars.to_a.permutation.map(&:join).sort[1e6-1].to_i
+# => 2783915460
