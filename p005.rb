920c970b5a82790675cd25a4415f12fdb0a2e44b
Tim Yang <timothy.d.yang@gmail.com>
2014-02-15 21:40:29 -0600

    redo as Numeric#divisible_upto?

--- p005.rb
+++ p005.rb
@@ -3,23 +3,18 @@
 # Smallest multiple
 # http://projecteuler.net/problem=5
 #
-# 2520 is the smallest number that can be divided by each of the numbers
-# from 1 to 10 without any remainder.
+# 2520 is the smallest number that can be divided by each of the numbers from
+# 1 to 10 without any remainder.
 #
 # What is the smallest positive number that is evenly divisible by all of the
 # numbers from 1 to 20?
 #
 
-def all_divisible?(dend, sors)
-  sors.each {|sor| return false unless dend % sor == 0 } or true
+class Numeric
+  def divisible_upto?(max)
+    (1..max).all? {|x| modulo(x).zero? }
+  end
 end
 
-def smallest_multiple_upto(max)
-  sors = 1..max
-  dend = sors.last + 1
-  dend += 1 until all_divisible?(dend, sors)
-  dend
-end
-
-p smallest_multiple_upto(20)
+p (1..Float::INFINITY).find {|x| x.divisible_upto?(20) }
 # => 232792560
