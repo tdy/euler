487a5461b042d46671bbc1d0af3714c4242a1eab
Tim Yang <timothy.d.yang@gmail.com>
2014-02-20 17:51:36 -0600

    redo as Numeric#sum_square and Numeric#square_sum

--- p006.rb
+++ p006.rb
@@ -4,10 +4,10 @@
 # http://projecteuler.net/problem=6
 #
 # The sum of the squares of the first ten natural numbers is,
-#   1^2 + 2^2 + ... + 10^2 = 385
+#   1**2 + 2**2 + ... + 10**2 = 385
 #
 # The square of the sum of the first ten natural numbers is,
-#   (1 + 2 + ... + 10)^2 = 55^2 = 3025
+#   (1 + 2 + ... + 10)**2 = 55**2 = 3025
 #
 # Hence the difference between the sum of the squares of the first ten natural
 # numbers and the square of the sum is 3025 − 385 = 2640.
@@ -16,19 +16,15 @@
 # natural numbers and the square of the sum.
 #
 
-class Array
-  def sum
-    inject(:+)
+class Numeric
+  def sum_square
+    downto(1).inject(0) {|sum, x| sum + x*x }
+  end
+
+  def square_sum
+    downto(1).inject(:+) ** 2
   end
 end
 
-def sum_square_upto(max)
-  (1..max).map {|x| x ** 2 }.sum
-end
-
-def square_sum_upto(max)
-  (1..max).to_a.sum ** 2
-end
-
-p (sum_square_upto(100) - square_sum_upto(100)).abs
+p 100.square_sum - 100.sum_square
 # => 25164150
