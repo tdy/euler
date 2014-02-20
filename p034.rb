dd6415260c609458bb322ca751719abe6e44c256
Tim Yang <timothy.d.yang@gmail.com>
2014-02-19 18:07:56 -0600

    tweak desc

--- p034.rb
+++ p034.rb
@@ -7,7 +7,7 @@
 #
 # Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 #
-# Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+# NOTE: as 1! = 1 and 2! = 2 are not sums they are not included.
 #
 
 class Integer
